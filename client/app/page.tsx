2ef798bbdf27a9f6c4831aee1c8b87b9f34f8b6c
kushashar-4 <110451154+kushashar-4@users.noreply.github.com>
2023-12-17 14:40:36 -0500

    landing page setup

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -36,20 +36,54 @@ export default function Hero() {
               </div>
               <a>
                 <Button size="lg" className="bg-brand font-bold">
-                  Learn More
+                  Our Programs
                 </Button>
               </a>
             </div>
             <Image src="images/seniors.jpg" width={500} isZoomed></Image>
           </div>
         </section>
-        <Modal>
-          <ModalHeader>Hello Michael</ModalHeader>
-          <ModalBody>
-            Hello MichaelHello MichaelHello MichaelHello MichaelHello
-            MichaelHello MichaelHello Michael
-          </ModalBody>
-        </Modal>
+        <section className="flex flex-col items-center w-full">
+          <div className="flex flex-col items-center gap-6">
+            <div className="flex flex-col items-center gap-4">
+              <p className="text-4xl font-bold text-white">About Us</p>
+              <p className="text-lg font-medium text-white max-w-2xl text-center">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.
+              </p>
+            </div>
+            <Button
+              className="bg-bgbrand text-black px-6 border-brand text-white"
+              size="md"
+              variant="bordered"
+            >
+              Learn More
+            </Button>
+          </div>
+        </section>
+        <section className="flex justify-center items-center gap-[130px] w-full bg-brand py-12">
+          <div className="flex flex-col gap-7">
+            <p className="text-4xl text-bgbrand font-bold">Our Mission</p>
+            <div className="flex flex-col gap-4">
+              <p className="text-lg text-bgbrand font-medium max-w-lg">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
+                maxime non, velit odio totam incidunt tempora impedit, iste quam
+                veniam dolor doloremque quas hic aliquam dolorum sint minus
+                corporis molestias, repudiandae nihil?
+              </p>
+              <Button size="md" className="bg-bgbrand text-white w-max">
+                About Us
+              </Button>
+            </div>
+          </div>
+          <Image
+            src="https://images.unsplash.com/photo-1508963493744-76fce69379c0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            isZoomed
+            width={400}
+          ></Image>
+        </section>
       </div>
     </>
   );
